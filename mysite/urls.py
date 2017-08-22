a9fd6faa10974eff79fe3ed29f872854e1778c8f
Timothy Olaleke <timothyolaleke@gmail.com>
2017-08-22 21:08:02 +0100

    Implemented Redirect and Fixed DetailView

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url (r'^polls/' ,include('polls.urls')),
+    url(r'^', RedirectView.as_view(url='polls/'))    
 ]
